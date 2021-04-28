b2ef04342835a67ad785b2cde69410c73e434af5
julian petrescu <julian.petrescu@live.com.sg>
2021-04-28 20:10:28 +0800

    First version, all working

---
+++ app_restful.py
@@ -0,0 +1,114 @@
+# Using helper methods from Flask for more RESTful APIs
+#!flask/bin/python
+
+from flask import Flask
+from flask_restful import Api, Resource
+from flask_restful import reqparse
+from flask_restful import fields, marshal
+from flask_httpauth import HTTPBasicAuth
+
+auth = HTTPBasicAuth()
+
+task_fields = {
+    'title': fields.String,
+    'description': fields.String,
+    'done': fields.Boolean,
+    'uri': fields.Url('task')
+}
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
+
+app = Flask(__name__)
+api = Api(app)
+
+class TaskListAPI(Resource):
+    def get(self):
+        pass
+
+    def post(self):
+        pass
+
+class UserAPI(Resource):
+    decorators = [auth.login_required]
+
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('title', type = str, required = True,
+            help = 'No task title provided', location = 'json')
+        self.reqparse.add_argument('description', type = str, default = "", location = 'json')
+        super(UserAPI, self).__init__()
+
+    def get(self, id):
+        pass
+
+    def put(self, id):
+        pass
+
+    def delete(self, id):
+        pass
+
+class TaskAPI(Resource):
+    decorators = [auth.login_required]
+
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('title', type = str, location = 'json')
+        self.reqparse.add_argument('description', type = str, location = 'json')
+        self.reqparse.add_argument('done', type = bool, location = 'json')
+        super(TaskAPI, self).__init__()
+
+    def get(self, id):
+        pass
+
+    def put(self, id):
+        task = filter(lambda t: t['id'] == id, tasks)
+        if len(task) == 0:
+            abort(404)
+        task = task[0]
+        args = self.reqparse.parse_args()
+        for k, v in args.iteritems():
+            if v != None:
+                task[k] = v
+        return { 'task': marshal(task, task_fields) }
+
+    def delete(self, id):
+        pass
+
+api.add_resource(TaskListAPI, '/todo/api/v1.0/tasks', endpoint = 'tasks')
+api.add_resource(TaskAPI, '/todo/api/v1.0/tasks/<int:id>', endpoint = 'task')
+api.add_resource(UserAPI, '/users/<int:id>', endpoint = 'user')
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods = ['PUT'])
+@auth.login_required
+def update_task(task_id):
+    task = filter(lambda t: t['id'] == task_id, tasks)
+    if len(task) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'title' in request.json and type(request.json['title']) != unicode:
+        abort(400)
+    if 'description' in request.json and type(request.json['description']) is not unicode:
+        abort(400)
+    if 'done' in request.json and type(request.json['done']) is not bool:
+        abort(400)
+    task[0]['title'] = request.json.get('title', task[0]['title'])
+    task[0]['description'] = request.json.get('description', task[0]['description'])
+    task[0]['done'] = request.json.get('done', task[0]['done'])
+    return jsonify( { 'task': make_public_task(task[0]) } )
+
+if __name__ == '__main__':
+    app.run(debug=False)
